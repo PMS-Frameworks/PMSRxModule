c3d0fe4b06579ecc33113e4160a3c4abc5a09854
GoEun Jeong <gogo8272@gmail.com>
2021-06-19 21:46:09 +0900

    Modify Platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PMSRxModule",
-    Platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "PMSRxModule",
